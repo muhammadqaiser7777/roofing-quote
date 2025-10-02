0872238ec1ac3fb73e0c55416ac613bed34348cf
muhammadqaiser7777 <qaiserakram7777@gmail.com>
2025-10-02 16:22:00 +0500

    Complete Version 1.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
-import { Routes } from '@angular/router';
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Faq } from './pages/faq/faq';
+import { Form } from './pages/form/form';
+import { Policies } from './pages/policies/policies';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'about', component: About },
+  { path: 'contact', component: Contact },
+  { path: 'faq', component: Faq },
+  { path: 'form', component: Form },
+  { path: 'policies', component: Policies },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled', // 👈 scrolls to top automatically
+      anchorScrolling: 'enabled'           // optional: allows #anchor navigation
+    })
+  ],
+  exports: [RouterModule] // 👈 needed so routing works
+})
+export class AppRoutingModule {}
